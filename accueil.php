71ec95c2935424bf6d7c03f1c311d4f17f6cd992
Mehdi <mehdiezzahriii@gmail.com>
2022-11-13 15:50:13 +0100

    L'ajout du page head et navbar et les inclure en pages acceul,login et singup

--- accueil.php
+++ accueil.php
@@ -1,54 +1,9 @@
 <?php
     include('connexion.php');
+    $Title='YouCode | Origin Gamer';
+    include('head.php');
+    include('navbar.php');
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>YouCode | Origin Gamer</title>
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500&display=swap">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/f57667c685.js" crossorigin="anonymous"></script>
-
-</head>
-<body>
-
-    <div class="d-flex align-items-center justify-content-between px-5 py-4">
-
-        <div>
-            <a href="accueil.php"><img class ="logo" src="assets/images/logos/YC-gamer 1.png" alt="logo" width="200"></a>
-        </div>
-
-        <div class="d-flex">
-            <input type="text" class="form-control border border-0" placeholder="Trouver des jeux" />
-            <button class="btn btn-search"><i class="fa fa-search"></i></button>
-        </div>
-
-        <div class="lien">
-            <a class="text-decoration-none" href="#">Acheter vos jeux</a>
-        </div>
-
-        <div class="lien">
-            <a class="text-decoration-none" href="login.php">Connectez-vous</a>
-        </div>
-
-        <div class="lien">
-            <a class="text-decoration-none" href="signup.php">Inscrivez-vous</a>
-        </div>
-
-        <div class="lien">   
-            <a href="#"><i class="fa-solid fa-heart text"></i></a>
-            <a href="#"><i class="fa-solid fa-cart-shopping ps-4"></i></a>
-        </div>
-
-    </div>
 
     <div class="liens d-flex align-items-center justify-content-between bg-dark px-5 py-2 mb-3">
         <a href="#" class="font-monospace text-decoration-none">Jeux Pc</a>
---
+++ navbar.php
@@ -0,0 +1,31 @@
+<body>
+
+    <div class="d-flex align-items-center justify-content-between px-5 py-4 mb-5">
+
+        <div>
+            <a href="accueil.php"><img class ="logo" src="assets/images/logos/YC-gamer 1.png" alt="logo" width="200"></a>
+        </div>
+
+        <div class="d-flex">
+            <input type="text" class="form-control border border-0" placeholder="Trouver des jeux" />
+            <button class="btn btn-search"><i class="fa fa-search"></i></button>
+        </div>
+
+        <div class="lien">
+            <a class="text-decoration-none" href="#">Acheter vos jeux</a>
+        </div>
+
+        <div class="lien">
+            <a class="text-decoration-none" href="login.php">Connectez-vous</a>
+        </div>
+
+        <div class="lien">
+            <a class="text-decoration-none" href="signup.php">Inscrivez-vous</a>
+        </div>
+
+        <div class="lien">   
+            <a href="#"><i class="fa-solid fa-heart text"></i></a>
+            <a href="#"><i class="fa-solid fa-cart-shopping ps-4"></i></a>
+        </div>
+
+    </div>
