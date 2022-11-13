7bd98b6d0d396388a0458b43b7029df51e3580a7
Mehdi <mehdiezzahriii@gmail.com>
2022-11-13 15:35:34 +0100

    Connexion réusite entre php et database

---
+++ connexion.php
@@ -0,0 +1,13 @@
+<?php
+
+$server_name    ="localhost";
+$user           ="root";
+$password       ="";
+$database_name  ="origingamer";
+
+$connect    =mysqli_connect($server_name,$user,$password,$database_name);
+if(!$connect){
+    die('Erreur :' .mysqli_connect_error());
+}
+
+?>
