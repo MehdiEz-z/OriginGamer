91ec4a3d44b5c8ebeff17214b55c9ed7625a29a0
Mehdi <mehdiezzahriii@gmail.com>
2022-11-14 01:36:57 +0100

    Etape connexion -login- effectué et l'affichage du mail sur la page test dashboard

--- login.php
+++ login.php
@@ -2,34 +2,61 @@
     include('connexion.php');
     $Title='Login | Origin Gamer';
     include('head.php');
-    include('navbar.php');
+    include('navbar.php'); 
+
+    session_start();
+
+    if(isset($_POST['login'])){
+
+        $email      = mysqli_real_escape_string($connect, $_POST['email']);
+        $password   = md5($_POST['password']);
+
+        $requete    = "SELECT * FROM user_infos WHERE email = '$email' && password = '$password' ";
+        $query      = mysqli_query($connect, $requete);
+
+        if(mysqli_num_rows($query) > 0){
+            $_SESSION['email'] = $email;
+            header('location:test.php');
+        }else{
+            $error[] = 'Email ou mot de passe incorect'; 
+        }
+    };
+?>
 ?>
 
     <div class="vw-100 row d-flex justify-content-center mt-5">
         
         <div class="col-lg-4 col-md-6 col-sm-10 d-flex flex-column text-center">
-            <div class="mt-5 mb-4">
+            <div class="mt-5 mb-5">
                 <h2>CONNECTEZ-VOUS</h2>
             </div>
-            <div class="lien mb-5">
-                Vous n'avez pas encore de compte ?
-                <a href="signup.php" class="text-decoration-none">Inscrivez-vous</a>
-            </div>
+            <?php
+            if(isset($error)){
+                foreach($error as $error){
+                    echo '<span class="d-block p-2 rounded-2 text-white mb-4" style="background: rgba(177,79,252,1);">'.$error.'</span>';
+                };
+            };  
+            ?>
+        <form action="" method="post">  
             <div class="mb-3 text-start">
                 <label class="col-form-label fw-light">Email*</label>
-                <input class="form-control" type="email" placeholder="mail@website.com" required>
+                <input class="form-control" name="email" type="email" placeholder="mail@website.com" required>
             </div>
             <div class="mb-4 text-start">
                 <label class="col-form-label fw-light">Mot de passe*</label>
-                <input class="form-control" type="password" placeholder="mot de passe" required>
+                <input class="form-control" name="password" type="password" placeholder="mot de passe" required>
             </div>
             <div class="lien mb-4 text-end">
                 <a href="#" class="text-decoration-none">Mot de passe oublié ?</a>					
             </div>
             <div class="mb-3">
-                <input type="submit" value="Connectez-vous" class="login-btn rounded-pill border-0 py-3">
+                <input type="submit" name="login" value="Connectez-vous" class="login-btn rounded-pill border-0 py-3">
             </div>
-            
+            <div class="lien mb-5">
+                Vous n'avez pas encore de compte ?
+                <a href="signup.php" class="text-decoration-none">Inscrivez-vous</a>
+            </div>
+        </form>     
         </div>
     </div>
 </body>
--- test.php
+++ test.php
@@ -1,7 +1,13 @@
 <?php
+include('connexion.php');
 $Title='Dashboard | Origin Gamer';
 include('head.php');
 
+session_start();
+
+if(!isset($_SESSION['email'])){
+    header('location:test.php');
+}
 ?>
 
 <body>
@@ -10,7 +16,7 @@ include('head.php');
             <h3 class="pb-3">salut, <span class="text-white rounded-3 px-3" style="background: rgba(177,79,252,1);">admin</span></h3>
             <h1>BIENVENU</h1>
             <p class="py-2 fs-4">c'est votre page admin</p>
-            <p class="pb-2 fw-bold">votre email : <span class="text-danger"></span></p>
+            <p class="pb-2 fw-bold">votre email : <span class="text-danger"><?php echo $_SESSION['email'] ?></span></p>
             <a href="login.php" class="logout text-decoration-none d-inline-block mt-2 text-white rounded-3 py-2 px-4" style="background: linear-gradient(90deg, rgba(177,79,252,1) 0%, rgba(130,74,248,1) 100%);">Déconnexion</a>
         </div>
     </div>
